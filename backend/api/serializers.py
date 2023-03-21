8c9009435c01a6492b5ed10153b6157a3ef550f7
Дмитрий Кох <dnkdnk7@yandex.ru>
2023-03-21 17:08:10 +0300

    download_shopping_cart_issue

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -58,9 +58,9 @@ class CustomUserSerializer(UserSerializer):
 
     def get_is_subscribed(self, author):
         user = self.context.get('request').user
-        return (user.is_authenticated
-                and Subscribe.objects.filter(user=user, author=author).exists()
-                )
+        if user.is_anonymous:
+            return False
+        return Subscribe.objects.filter(user=user, author=author).exists()
 
 
 class SubscribeSerializer(CustomUserSerializer):
@@ -134,8 +134,8 @@ class RecipeReadSerializer(ModelSerializer):
     author = CustomUserSerializer(read_only=True)
     ingredients = IngredientInRecipeSerializer(many=True)
     image = Base64ImageField()
-    is_favorited = SerializerMethodField(read_only=True)
-    is_in_shopping_cart = SerializerMethodField(read_only=True)
+    is_favorited = SerializerMethodField()
+    is_in_shopping_cart = SerializerMethodField()
 
     class Meta:
         model = Recipe
@@ -154,15 +154,15 @@ class RecipeReadSerializer(ModelSerializer):
 
     def get_is_favorited(self, recipe):
         user = self.context.get('request').user
-        return (user.is_authenticated
-                and user.favorites.filter(recipe=recipe).exists()
-                )
+        if user.is_authenticated:
+            return False
+        return user.favorites.filter(recipe=recipe).exists()
 
     def get_is_in_shopping_cart(self, recipe):
         user = self.context.get('request').user
-        return (user.is_authenticated
-                and user.favorites.filter(recipe=recipe).exists()
-                )
+        if user.is_anonymous:
+            return False
+        return user.shopping_cart.filter(recipe=recipe).exists()
 
 
 class RecipeWriteSerializer(ModelSerializer):
